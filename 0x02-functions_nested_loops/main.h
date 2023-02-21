20bf44140ca1d40348cd3586c51b7cbe1e27af7e
Official0mega <supr3m30m3ga@gmail.com>
2023-02-20 23:28:01 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+print_last_digit(int);
+jack_bauer(void);
+times_table(void);
+add(int, int);
+print_to_98(int n);
+print_times_table(int n);
+
+#endif
