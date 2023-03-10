1749d0c2d85dd99f52952dbf432f55a55bb82eac
Official0mega <supr3m30m3ga@gmail.com>
2023-03-10 07:54:38 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,17 @@
-#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void) argc;
-printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
-return (0);
+	return (0);
 }
